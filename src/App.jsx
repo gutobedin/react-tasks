ca9584b3351edfbc3f9418e3e93520f83f9f6ad5
Gustavo Bedin <gustavobedin@Gustavos-MacBook-Pro.local>
2022-08-20 22:20:31 -0300

    Criação Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import {useEffect, useState} from 'react'
+import { Header } from './components/Header'
+
+import './global.css'
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Header/>
   )
 }
 
